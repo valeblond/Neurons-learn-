15e3456a801a071f116effa43ddeeae1622170e4
U-DELL9\валера <valera_9796@mail.ru>
2019-07-26 16:54:29 +0200

    BM

--- !myDeepLearning/BM/rbm.py
+++ !myDeepLearning/BM/rbm.py
@@ -24,7 +24,7 @@ ratings = pd.read_csv('ml-1m/ratings.dat', sep='::', header=None, engine='python
 training_set = pd.read_csv('ml-100k/u1.base', delimiter='\t')
 training_set = np.array(training_set, dtype='int')
 test_set = pd.read_csv('ml-100k/u1.test', delimiter='\t')
-test_set = np.array(training_set, dtype='int')
+test_set = np.array(test_set, dtype='int')
 
 #Getting the number of users and movies
 nb_users = int(max(max(training_set[:, 0]), max(test_set[:, 0])))
@@ -57,7 +57,7 @@ test_set[test_set == 1] = 0
 test_set[test_set == 2] = 0
 test_set[test_set >= 3] = 1
 
-#Creatong the architecture of the Neural Network
+#Creating the architecture of the Neural Network
 class RBM():
     def __init__(self, nv, nh):
         self.W = torch.randn(nh, nv)
@@ -70,10 +70,50 @@ class RBM():
         return p_h_given_v, torch.bernoulli(p_h_given_v)
     def sample_v(self, y):
         wy = torch.mm(y, self.W)
-        activation = wy + self.a.expand_as(wy)
+        activation = wy + self.b.expand_as(wy)
         p_v_given_h = torch.sigmoid(activation)
         return p_v_given_h, torch.bernoulli(p_v_given_h)
+    def train(self, v0, vk, ph0, phk):
+        self.W += (torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)).t()
+        self.b += torch.sum((v0 - vk), 0)
+        self.a += torch.sum((ph0 - phk), 0)
 
+nv = len(training_set[0])
+nh = 100
+batch_size = 100
+rbm = RBM(nv, nh)
+
+#Training the RBM
+nb_epoch = 10
+for epoch in range(1, nb_epoch + 1):
+    train_loss = 0
+    s = 0.
+    for id_user in range(0, nb_users - batch_size, batch_size):
+        vk = training_set[id_user:id_user+batch_size]
+        v0 = training_set[id_user:id_user+batch_size]
+        ph0,_ = rbm.sample_h(v0)
+        for k in range(10):
+            _,hk = rbm.sample_h(vk)
+            _,vk = rbm.sample_v(hk)
+            vk[v0<0] = v0[v0<0]
+        phk,_ = rbm.sample_h(vk)
+        rbm.train(v0, vk, ph0, phk)
+        train_loss += torch.mean(torch.abs(v0[v0>=0] - vk[v0>=0]))
+        s += 1.
+    print('epoch: ' + str(epoch) + ' loss: ' + str(train_loss/s))
+
+#Testing the RBM
+test_loss = 0
+s = 0.
+for id_user in range(nb_users):
+    v = training_set[id_user:id_user+1]
+    vt = test_set[id_user:id_user+1]
+    if len(vt[vt>=0]) > 0:
+        _,h = rbm.sample_h(v)
+        _,v = rbm.sample_v(h)
+        test_loss += torch.mean(torch.abs(vt[vt>=0] - v[vt>=0]))
+        s += 1.
+print('test loss: ' + str(test_loss/s))
 
 
 

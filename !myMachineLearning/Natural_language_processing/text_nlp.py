a68405fca154c6201d904f0e470ded50ec90af35
U-DELL9\валера <valera_9796@mail.ru>
2019-09-10 01:03:39 +0200

    new

--- !myMachineLearning/Natural_language_processing/text_nlp.py
+++ !myMachineLearning/Natural_language_processing/text_nlp.py
@@ -1,14 +1,14 @@
-# Natural Language Processing
+#NLP
 
-# Importing the libraries
+#Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+#Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting=3)
 
-# Cleaning the texts
+#Cleaning the texts
 import re
 import nltk
 nltk.download('stopwords')
@@ -24,15 +24,15 @@ for i in range(0, 1000):
     review = ' '.join(review)
     corpus.append(review)
 
-# Creating the Bag of Words model
+#Creating the Bag of Words model
 from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer(max_features = 1500)
+cv = CountVectorizer(max_features=1500)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
 # Fitting Naive Bayes to the Training set
 from sklearn.naive_bayes import GaussianNB
@@ -45,3 +45,4 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+print(cm)
